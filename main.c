f3d45dfb393e36b354fe5d7c9d1b096e32f9d087
David-Lcswtx <davidlucswtx@gmail.com>
2024-11-23 20:19:13 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,82 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Livro{
+	
+	char nome[50];
+	char isbn[30];
+	float preco;
+	int score;
+	char editora[30];
+	
+};
+
+
+typedef struct Livro* pLivro;
+
+pLivro livro_aloc(int qtde){
+	
+	pLivro livros = (pLivro)malloc(qtde * sizeof(struct Livro));
+	
+	return livros;
+	
+}
+
+void livro_ler(pLivro livros, int qtde){
+	
+	int i;
+	
+	for (i = 0; i < qtde; i++){
+		scanf(" %[^\n]", livros[i].nome);
+		scanf("%s", livros[i].isbn);
+		scanf("%f", &livros[i].preco);
+		scanf("%d", &livros[i].score);
+		scanf(" %[^\n]", livros[i].editora);
+	}
+	
+}
+
+void livro_exibe(pLivro livros, int qtde){
+	
+	int i;
+	
+	for (i = 0; i < qtde; i++){
+		
+		printf("Livro %d:\n", i + 1);
+		printf("Nome: %s\n", livros[i].nome);	
+		printf("ISBN: %s\n", livros[i].isbn);
+		printf("Preço: R$ %.2f\n", livros[i].preco);
+		printf("Score: %d\n", livros[i].score);
+		printf("Editora: %s\n", livros[i].editora);
+		
+		if(i < qtde - 1){
+			printf("\n");
+		}
+		
+	}
+	
+}
+
+void livro_desaloca(pLivro livros){
+	
+	free(livros);
+	
+}
 
 int main(){
-
-    //implemente seu codigo
-    printf("Nao implementado ainda.");
-    
-    return 0;
+	
+	int quantidade;
+	
+	scanf("%d",&quantidade);
+	
+	pLivro livros = livro_aloc(quantidade);
+	
+	livro_ler(livros, quantidade);
+	
+	livro_exibe(livros, quantidade);
+	
+	livro_desaloca(livros);
+	
+	return 0;	
 }
